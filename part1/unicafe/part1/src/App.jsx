ab191ba6a2761175d2b4c2500454ad1d0744947c
BenjaminIndo <benjamin.indo@ug.uchile.cl>
2025-03-03 18:09:46 -0300

    Add exercise 1.8

--- part1/unicafe/part1/src/App.jsx
+++ part1/unicafe/part1/src/App.jsx
@@ -5,6 +5,26 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = (props) => {
+  const good = props.good
+  const neutral = props.neutral
+  const bad = props.bad
+  const all = props.all
+  const average = (good-bad)/all
+  const positive = (good/all)*100
+  return(
+    <div>
+    <h1>statistics</h1>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {all}</p>
+    <p>average {average}</p>
+    <p>positive {positive}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   const addGood = () =>{
     setGood(good+1)
@@ -26,23 +46,15 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
 
-  const average = (good-bad)/all
-  const positive = ((good/all)*100)
-
   return (
     <div>
       <h1>give feedback</h1>
       <Button handleClick={addGood} text = "good"/>
       <Button handleClick={addNeutral} text = "neutral"/>
       <Button handleClick={addBad} text = "bad"/>
-      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
 
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+
     </div>
   )
 }
