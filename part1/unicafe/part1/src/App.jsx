8d1c36b02b481f7ec7e09b8777c96a777390bb4e
BenjaminIndo <benjamin.indo@ug.uchile.cl>
2025-03-04 00:20:20 -0300

    Add exercise 1.11

--- part1/unicafe/part1/src/App.jsx
+++ part1/unicafe/part1/src/App.jsx
@@ -7,9 +7,10 @@ const Button = (props) => (
 
 const StatisticLine = (props) =>{
   return(
-    <div>
-      <p>{props.text} {props.value}</p>
-    </div>
+    <tr>
+      <td>{props.text} </td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 const Statistics = (props) => {
@@ -27,14 +28,16 @@ const Statistics = (props) => {
     )
   }
   return(
-    <div>
-    <StatisticLine text="good" value = {good}/>
-    <StatisticLine text="neutral" value = {neutral}/>
-    <StatisticLine text="bad" value = {bad}/>
-    <StatisticLine text="all" value = {all}/>
-    <StatisticLine text="average" value = {average}/>
-    <StatisticLine text="positive" value = {positive}/>
-    </div>
+    <table>
+      <tbody>
+      <StatisticLine text="good" value = {good}/>
+      <StatisticLine text="neutral" value = {neutral}/>
+      <StatisticLine text="bad" value = {bad}/>
+      <StatisticLine text="all" value = {all}/>
+      <StatisticLine text="average" value = {average}/>
+      <StatisticLine text="positive" value = {positive}/>
+      </tbody>
+    </table>
   )
 }
 
